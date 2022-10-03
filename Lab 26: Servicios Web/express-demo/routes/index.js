1c5e32480d5ba32f9e21c8e9859352a59ca99047
CVJJ <A01707735@itesm.mx>
2022-10-02 23:33:37 -0500

    lab26

---
+++ Lab 26: Servicios Web/express-demo/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', (req, res) => {
+    console.log(req.oidc.isAuthenticated());
+    res.render('index', {
+        title: "Express Demo",
+        isAuthenticated : req.oidc.isAuthenticated(),
+        user: req.oidc.user,
+    });
+});
+
+module.exports = router;
